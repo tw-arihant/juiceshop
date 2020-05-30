2feeae30895715f956c23336c5c2bff33b8212ab
Arihant Jain <arihant.jain@thoughtworks.com>
2020-05-30 17:12:15 +0530

    Adding the dockerfiles and jenkins file

--- src/test/java/JuiceShopE2ETest.java
+++ src/test/java/JuiceShopE2ETest.java
@@ -1,22 +1,38 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
-public class juiceshop_e2e {
+
+public class JuiceShopE2ETest {
 
     WebDriver driver;
     String email = "b@gmail.com";
     String password = "abc@gmail.com";
 
     @Test
-    public void register(){
-        System.setProperty("webdriver.chrome.driver","/Users/swati.kalra/Downloads/chromedriver");
-        driver = new ChromeDriver();
+    public void register() {
+        WebDriverManager.chromedriver().setup();
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://hub.com:4444/wd/hub"),
+                    capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.manage().window().maximize();
-        driver.get("http://localhost:3000/#/register");
+        driver.get("http://juice-shop:3000/#/register");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/app-welcome-banner/div/div[2]/button[2]")).click();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
@@ -30,23 +46,21 @@ public class juiceshop_e2e {
         driver.findElement(By.id("securityAnswerControl")).sendKeys(password);
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         driver.findElement(By.id("registerButton")).click();
-
+        driver.quit();
 
     }
 
     @Test
-    public void login(){
-    
-    
-    
-    
-    }
+    public void login() {
 
-    public void addItemToCart(){
 
     }
 
-    public void removeItemFromCart(){
+    public void addItemToCart() {
+
+    }
+
+    public void removeItemFromCart() {
 
     }
 
